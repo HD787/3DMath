aedb5f5099629a2f084d239cf8455d2aefc7b6f4
henry <Hd116527@gmail.com>
2024-04-19 13:28:07 -0500

    unstested but compiles, got to decide where and where not to use pointers in operations, leaning towards not using them at all as these are small structs

--- operations.h
+++ operations.h
@@ -2,14 +2,14 @@
 #include "types.h"
 //Matrix operations
 void vecByMatrix4x4(vec4* vec, matrix4x4 matrix){
-    float x = vec->x * matrix[0] + vec->y * matrix[4] + vec->z * matrix[8] + vec->real * matrix[12];
-    float y = vec->x * matrix[1] + vec->y * matrix[5] + vec->z * matrix[9] + vec->real * matrix[13];
-    float z = vec->x * matrix[2] + vec->y * matrix[6] + vec->z * matrix[10] + vec->real * matrix[14];
-    float real = vec->x * matrix[3] + vec->y * matrix[7] + vec->z * matrix[11] + vec->real * matrix[15];
+    float x = vec->x * matrix[0] + vec->y * matrix[4] + vec->z * matrix[8] + vec->w * matrix[12];
+    float y = vec->x * matrix[1] + vec->y * matrix[5] + vec->z * matrix[9] + vec->w * matrix[13];
+    float z = vec->x * matrix[2] + vec->y * matrix[6] + vec->z * matrix[10] + vec->w * matrix[14];
+    float w = vec->x * matrix[3] + vec->y * matrix[7] + vec->z * matrix[11] + vec->w * matrix[15];
     vec->x = x;
     vec->y = y;
     vec->z = z;
-    vec->real = real;
+    vec->w = w;
 }
 
 //idk if i love these names as the types and the parametes follow the same scheme
@@ -32,7 +32,7 @@ void normalizeQuatertion(quaternion* quat){
    quat->k /= normalVal; 
 }
 
-quaternion* multiplyQuaternion(quaternion* quat1, quaternion* quat2){
+quaternion multiplyQuaternion(quaternion* quat1, quaternion* quat2){
     quaternion temp;
     temp.real = (quat1->real * quat2->real - quat1->i * quat2->i - quat1->j * quat2->j - quat1->k + quat2->k);
     temp.i = (quat1->real * quat2->i + quat1->i * quat2->real + quat1->j * quat2->k - quat1->k + quat2->j);
@@ -41,7 +41,7 @@ quaternion* multiplyQuaternion(quaternion* quat1, quaternion* quat2){
     return temp;
 }
 
-quaternion* createConjugate(quaternion* quat){
+quaternion createConjugate(quaternion quat){
     quaternion temp;
     temp.real = quat.real;
     temp.i = quat.i * -1;
@@ -50,7 +50,7 @@ quaternion* createConjugate(quaternion* quat){
     return temp;
 }
 
-quaternion* vectorToQuaternion(vec3 vec){
+quaternion vectorToQuaternion(vec3 vec){
     quaternion temp;
     temp.real = 0;
     temp.i = vec.x;
@@ -59,19 +59,19 @@ quaternion* vectorToQuaternion(vec3 vec){
     return temp;
 }
 
-vec3* quaternionToVector(quaternion quat){
+vec3 quaternionToVector(quaternion quat){
     vec3 temp;
     temp.x = quat.i;
     temp.y = quat.j;
     temp.z = quat.k;
-    return temp
+    return temp;
 }
 
-vec3* rotateVectorViaQuaternion(vec3* vec, quaternion* quat){
-    quaternion* vquat = vectorToQuaternion(vec);
-    quaternion* conjugate = createConjugate(quat)
-    quaternion* temp = multiplyQuaternion(quat, vquat);
-    quaternion* resultQuat = multiplyQuaternion(temp, conjugate);
-    vec3* result = quaternionToVector(resultQuat);
+vec3 rotateVectorViaQuaternion(vec3* vec, quaternion* quat){
+    quaternion vquat = vectorToQuaternion(*vec);
+    quaternion conjugate = createConjugate(*quat);
+    quaternion temp = multiplyQuaternion(quat, &vquat);
+    quaternion resultQuat = multiplyQuaternion(&temp, &conjugate);
+    vec3 result = quaternionToVector(resultQuat);
     return result;
 }
