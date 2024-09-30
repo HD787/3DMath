f4955dcd59232c12e86a136e6ef2579b3c718757
henry <Hd116527@gmail.com>
2024-09-30 13:18:28 -0500

    matrix changes

--- matrices.c
+++ matrices.c
@@ -167,9 +167,9 @@ void createNDCToScreenSpaceMatrix_COLUMNMAJOR(float width, float height, float m
     matrix[3] = 0.0f;               matrix[7] = 0.0f;               matrix[11] = 0.0f;              matrix[15] = 1.0f;
 }
 
-float[16] generateIdentityMatrix(){
-    float[16] matrix;
-    matrix[0] = 1.0f;  matrix[1] = 0.0f;  matrix[2] = 0.0;   matrix[3] = 0.0f;
+float* generateIdentityMatrix(){
+    float* matrix = malloc(sizeof(float) * 16);
+    matrix[0] = 1.0f;  matrix[1] = 0.0f;  matrix[2] = 0.0f;   matrix[3] = 0.0f;
     matrix[4] = 0.0f;  matrix[5] = 1.0f;  matrix[6] = 0.0f;  matrix[7] = 0.0f;
     matrix[8] = 0.0f;  matrix[9] = 0.0f;  matrix[10] = 1.0;  matrix[11] = 0.0f;
     matrix[12] = 0.0f; matrix[13] = 0.0f; matrix[14] = 0.0f; matrix[15] = 1.0f;
